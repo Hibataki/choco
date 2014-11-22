e7ff944cfd4d3cd2befa43eaf98386b1df83a7a7
hibataki <hibataki.samidare@gmail.com>
2014-11-22 16:31:56 +0900

    listがｱになる問題を修正

--- choco/src/main/java/choco/Bot.java
+++ choco/src/main/java/choco/Bot.java
@@ -9,16 +9,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
 public class Bot implements Runnable {
+	private static final Logger LOGGER = Logger.getLogger("Bot");
+	private static final long HOUR = 3600000;
+	private static final Random RANDOM = new Random(new Random().nextLong());
+
 	private final Twitter twitter;
 	private final long sleepSecond;
 	private final File file;
+
 	private boolean closed = false;
-	private List<String> list = new ArrayList<>();
+	private List<String> list;
 
 	public Bot(Twitter twitter, int sleepSecond, File file) {
 		this.twitter = twitter;
@@ -29,21 +36,23 @@ public class Bot implements Runnable {
 	@Override
 	public void run() {
 		while (!closed) {
+			list = new ArrayList<>();
 			try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
 				String line;
 				while ((line = br.readLine()) != null) {
 					list.add(line.replaceAll("\\\\n", "\n"));
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			Collections.shuffle(list);
-			for (int i = 0; i < 6; i++) {
-				try {
-					twitter.updateStatus(list.get(i));
+				Collections.shuffle(list);
+				LOGGER.info(() -> "Loading TextFile");
+
+				long now = 0;
+				while (now < HOUR) {
+					twitter.updateStatus(list.get(RANDOM.nextInt(list.size())));
 					Thread.sleep(sleepSecond);
-				} catch (InterruptedException | TwitterException e) {
+					now += sleepSecond;
 				}
+			} catch (IOException | TwitterException | InterruptedException e) {
+				LOGGER.warning(() -> e.getMessage());
 			}
 		}
 	}
--- choco/src/main/java/choco/BotStreamListener.java
+++ choco/src/main/java/choco/BotStreamListener.java
@@ -21,6 +21,7 @@ public final class BotStreamListener extends UserStreamAdapter {
 	private final Twitter twitter;
 	private final long id;
 	private final File file;
+	private Status status;
 
 	public BotStreamListener(Twitter twitter, long id, File file) {
 		this.twitter = twitter;
@@ -30,17 +31,22 @@ public final class BotStreamListener extends UserStreamAdapter {
 
 	@Override
 	public void onStatus(Status status) {
+		this.status = status;
+		checkText();
+	}
+
+	private void checkText() {
 		String text = status.getText();
 		if (text != null && !text.contains("RT")) {
 			try {
 				if (status.getUser().getId() == id) {
-					adminCommand(text, status);
+					adminCommand();
 				} else if (text.contains("(@hai_choco_agano") && text.endsWith(")")) {
-					twitter.updateStatus(getStatus(status, UpdateName.name(twitter, text)));
+					getStatus(UpdateName.name(twitter, text));
 				} else if (text.contains("@hai_choco_agano チョコ")) {
-					twitter.updateStatus(getStatus(status, Choco.choco()));
+					getStatus(Choco.choco());
 				} else if (text.contains("@hai_choco_agano gemochi")) {
-					twitter.updateStatus(getStatus(status, Gemochi.gemochi()));
+					getStatus(Gemochi.gemochi());
 				}
 			} catch (TwitterException e) {
 				LOGGER.info(() -> e.getMessage());
@@ -48,18 +54,19 @@ public final class BotStreamListener extends UserStreamAdapter {
 		}
 	}
 
-	private void adminCommand(String text, Status status) throws TwitterException {
+	private void adminCommand() throws TwitterException {
+		String text = status.getText();
 		if (text.startsWith("add")) {
 			text = text.replaceAll("\r", "").replaceAll("\n", "\\\\n").substring(4);
-			twitter.destroyStatus(status.getId());
 			try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), StandardCharsets.UTF_8))) {
 				bw.write(text);
 				bw.newLine();
-				twitter.sendDirectMessage(id, text.substring(0, 20) + "...のツイートが正常に登録されました");
+				twitter.sendDirectMessage(id, "「" + text.substring(0, 20) + "...」のツイートが正常に登録されました");
 			} catch (IOException e) {
 				LOGGER.warning(() -> e.getMessage());
-				twitter.sendDirectMessage(id, text.substring(0, 20) + "...のツイートの登録に失敗しました");
+				twitter.sendDirectMessage(id, "「" + text.substring(0, 20) + "...」のツイートの登録に失敗しました");
 			}
+			twitter.destroyStatus(status.getId());
 		}
 		// TODO delete, editの追加
 	}
@@ -76,7 +83,7 @@ public final class BotStreamListener extends UserStreamAdapter {
 		}
 	}
 
-	private StatusUpdate getStatus(Status status, String str) {
-		return new StatusUpdate("@" + status.getUser().getScreenName() + " " + str).inReplyToStatusId(status.getId());
+	private void getStatus(String str) throws TwitterException {
+		twitter.updateStatus(new StatusUpdate("@" + status.getUser().getScreenName() + " " + str).inReplyToStatusId(status.getId()));
 	}
 }
